6482e9d692acfbbb31b7d5c853044e72a4d4bb21
Xiaohu Guo <Xiaohu.Guo@amd.com>
2024-04-05 15:47:24 +0000

    add tmux init and change name to dotinit

--- dotinit.sh
+++ dotinit.sh
@@ -6,3 +6,6 @@ vim +PluginInstall +qall
 
 cd ~/.vim/bundle/YouCompleteMe/
 python3 install.py --clangd-completer
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
